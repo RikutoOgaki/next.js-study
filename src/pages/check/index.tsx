a74c1c8bef74269c7f414366c919b0ae2efd0b0b
Rikuto <orikutobasuke@gmail.com>
2023-03-03 16:17:40 +0900

    nextjs study now

--- src/pages/check/index.tsx
+++ src/pages/check/index.tsx
@@ -5,13 +5,7 @@ import Style from '@/styles/check/index.module.css'
 // styleの内容をstateで管理する
 // 直接cssを当てる
 
-
-
 export default function Check(){
-
-    const [state,setState] = useState({
-        
-    })
 
     return(
         <div className={Style.check}>
