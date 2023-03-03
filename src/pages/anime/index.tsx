fc014f82b8c44a0a3f171b871fefa1fd203c0881
Rikuto <orikutobasuke@gmail.com>
2023-03-03 13:17:38 +0900

    nextjs add

--- src/pages/anime/index.tsx
+++ src/pages/anime/index.tsx
@@ -4,7 +4,26 @@ export default function Anime(){
 return(
 <>
     <h1 className={styles.font}>animationについて</h1>
+    <div className={styles.demo}>
+        <h2 className={styles.font1}>transitionを使ったアニメーション</h2>
+        <p className={styles.ptag}>transition</p>
+        <div>
+            <div>box</div>
+            <div>box</div>
+            <div>box</div>
+            <div>box</div>
+        </div>
+    </div>
+    <div className={styles.demo2}>
+        <h2 className={styles.font1}>@keyframeを使ったアニメーション</h2>
+        <p className={styles.ptag}>@keyframe</p>
+        <div>
+            <div>box</div>
+        </div>
+    </div>
     <div className={styles.wrap}>
+        <h2 className={styles.font1}>animationの応用</h2>
+        <p className={styles.ptag}>ずっとくるくると回り続ける3D BOX</p>
         <div className={styles.dice}>
             <div>box1</div>
             <div>box2</div>
