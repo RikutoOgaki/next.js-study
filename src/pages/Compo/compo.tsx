154bdee3912d2198dbe0dd3bfd93f13f8cf3ff66
Rikuto <orikutobasuke@gmail.com>
2023-03-03 14:10:56 +0900

    nextjs study now

---
+++ src/pages/Compo/compo.tsx
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+import {Flex,Box,Center,Input,Text} from '@chakra-ui/react'
+
+export default function App(){
+    return(
+        <>
+            <Text as={'h1'}>hello</Text>
+        </>
+    )
+}
