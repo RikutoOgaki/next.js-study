216abd3d8fc940ab0ccbadd7e2a51ca2ceb23803
Rikuto <orikutobasuke@gmail.com>
2023-03-05 17:55:17 +0900

    nextjs study now

---
+++ src/pages/respons/index.tsx
@@ -0,0 +1,21 @@
+import {useState} from 'react'
+import Style from '@/styles/respons/index.module.css'
+
+export default function Respons(){
+    return(
+
+        <div>
+            <div>
+                <header className={Style.head}>
+                    <h1 className={Style.title}>Respons対応ページ</h1>
+                </header>
+                <main>
+                    
+                </main>
+                <footer>
+                    
+                </footer>
+            </div>
+        </div>
+    )
+}
